8510e71f4dd6245abe77f44939d7a7f855df1c85
RSakultarawattn <ray.sakultarawattn@gmail.com>
2020-09-27 15:12:56 -0700

    narrative

---
+++ app.js
@@ -0,0 +1,11 @@
+
+
+
+
+
+
+const input1 = inputName.value1;
+const input2 = inputTool.value2;
+const input3 = inputVerb.value3;
+const input4 = inputAdjective.value4;
+const input5 = inputAnimal.value5;
